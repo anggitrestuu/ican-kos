c56df3c7cecc17413780cfc4c75be33d45a1ea19
Novi Amanda Puspita Sari <apsnovi@gmail.com>
2023-05-21 15:17:38 +0700

    update profile

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'login_page.dart';
 import 'package:bwa_cozy/theme.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle regularTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+  fontWeight: FontWeight.w400,
+);
+
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -29,50 +36,70 @@ class ProfilePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const CircleAvatar(
-            radius: 80,
-            //backgroundImage: AssetImage('assets/noviamanda.jpeg'),
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            'Novi Amanda Puspita Sari',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 10),
-          const Text(
-            '123200065',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 10),
-          const Text(
-            'TPM IF - A',
-            style: TextStyle(fontSize: 16),
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            'Tempat dan Tanggal Lahir:',
-            style: TextStyle(fontSize: 16),
-          ),
-          const SizedBox(height: 10),
-          const Text(
-            'Blora, 02 November 2001',
-            style: TextStyle(fontSize: 16),
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            'Cita-cita:',
-            style: TextStyle(fontSize: 16),
-          ),
-          const SizedBox(height: 10),
-          const Text(
-            'Hidup tentram dan damai',
-            style: TextStyle(fontSize: 16),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 60,
+              backgroundImage: AssetImage('assets/noviamanda.jpeg'),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Novi Amanda Puspita Sari',
+              style: regularTextStyle.copyWith(
+                fontSize: 24,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              '123200065-TPM A',
+              style: regularTextStyle.copyWith(
+                fontSize: 20,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Cita-cita:',
+              style: regularTextStyle.copyWith(fontSize: 16),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Hidup tentram dan damai',
+              style: regularTextStyle.copyWith(fontSize: 16),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Kesan:',
+              style: regularTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Sangat menantang, banyak tugas dan keren sih. Harus pinter-pinter bagi waktu aja. Jujur saya blank waktu quiz, OMG gakuad.',
+              style: regularTextStyle.copyWith(fontSize: 16),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Pesan:',
+              style: regularTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Semoga kedepannya bisa lebih proaktif mahasiswanya, dan lebih dalam konsep pengajaran untuk teorinya.',
+              style: regularTextStyle.copyWith(fontSize: 16),
+            ),
+          ],
+        ),
       ),
     );
   }
